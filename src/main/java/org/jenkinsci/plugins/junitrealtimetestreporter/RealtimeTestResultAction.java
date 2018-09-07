9eaf0d8b11cef986c820a5006b576f931aa2a5ec
Daniel Beland <dcendents@gmail.com>
2018-09-07 13:13:36 -0400

    When parsing for realtime results, allow empty test results

--- src/main/java/org/jenkinsci/plugins/junitrealtimetestreporter/RealtimeTestResultAction.java
+++ src/main/java/org/jenkinsci/plugins/junitrealtimetestreporter/RealtimeTestResultAction.java
@@ -69,7 +69,7 @@ public class RealtimeTestResultAction extends AbstractRealtimeTestResultAction {
     @Override
     protected TestResult parse() throws IOException, InterruptedException {
         final JUnitResultArchiver archiver = getArchiver(this.owner);
-        return new JUnitParser(archiver.isKeepLongStdio(), /* TODO really?! */false).parse(getGlob(archiver), this.owner, null, null);
+        return new JUnitParser(archiver.isKeepLongStdio(), true).parse(getGlob(archiver), this.owner, null, null);
     }
 
     private String getGlob(final JUnitResultArchiver archiver) {
